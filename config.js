4e3e4c75d8f49601449cdb696f9c9af51b6b7016
Edo-Xex <97641029+peoplescurity@users.noreply.github.com>
2022-09-03 13:57:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 // Other
-global.owner = ['6285815975368']
-global.premium = ['6285815975368']
-global.ownername = 'Liya'
-global.botname = 'AliyaBot'
-global.packname = '© by AliyaBot'
+global.owner = ['6283867876689']
+global.premium = ['6283867876689']
+global.ownername = '3do'
+global.botname = '3do.bot'
+global.packname = '© by 3do.bot'
 global.gc = 'https://bit.ly/3KMlmv2'
-global.linkyt = 'https://youtube.com/channel/UC9Si3U0o9dGS9MDfJR5iF6Q'
-global.linkgc = 'https://chat.whatsapp.com/DwP6uHYBWBc6TBSsNJrzwN'
+global.linkyt = 'https://youtube.com/channel/UC9Si3U0o9dGS9MDfJR5iFQ'
+global.linkgc = 'https://chat.whatsapp.com/H3QHpWQcxSn2lwYvtg91c0'
 global.limitawal = '100'
 global.author = '@alya.xzy'
 global.sessionName = 'polo'
@@ -30,7 +30,7 @@ global.limitawal = {
     premium: "Infinity",
     free: 100
 }
-global.gambar = 'https://telegra.ph/file/7858fc96fffda74b3ab44.jpg' //disini gambar bot nya lu bisa ganti pink gambar ini dengan link gambar punya lu
+global.gambar = 'https://telegra.ph/file/262afe35004bd90fa5e61.jpg' //disini gambar bot nya lu bisa ganti pink gambar ini dengan link gambar punya lu
 global.sc = fs.readFileSync('./media/sc.jpg')
 global.tq = fs.readFileSync('./media/tq.jpg')
 global.menu = fs.readFileSync('./media/menu.jpg')
